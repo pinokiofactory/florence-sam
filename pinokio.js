26aff57f09b00384aec1cc0c3ee31ba325f164ff
cocktailpeanut <cocktailpeanuts@proton.me>
2024-08-01 10:15:33 -0400

    update

--- pinokio.js
+++ pinokio.js
@@ -2,7 +2,7 @@ const path = require('path')
 module.exports = {
   version: "2.0",
   title: "florence-sam",
-  description: "",
+  description: "Integrates Florence2 and SAM2 models for detailed image captioning and object detection. Florence2 generates detailed captions that are then used to perform phrase grounding. The Segment Anything Model 2 (SAM2) converts these phrase-grounded boxes into masks. https://huggingface.co/spaces/SkalskiP/florence-sam",
   icon: "icon.webp",
   menu: async (kernel, info) => {
     let installed = info.exists("app/env")
