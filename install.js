c2e5c6c04fa98a1b0ff4c1db4744d470ccf80014
cocktailpeanut <cocktailpeanuts@proton.me>
2024-08-01 11:13:22 -0400

    update

--- install.js
+++ install.js
@@ -25,7 +25,7 @@ module.exports = {
       when: "{{gpu === 'nvidia'}}",
       method: "shell.run",
       params: {
-        message: "conda install anaconda::cudnn"
+        message: "conda install -y anaconda::cudnn"
       }
     },
     // Edit this step with your custom install commands
